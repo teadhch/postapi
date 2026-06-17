9cda28fbaf6d5e81bbd5dcddb9ce5603ef2d4880
teadhch <teadhch@naver.com>
2026-06-17 11:40:18 +0900

    feat : post_router import include

--- app/main.py
+++ app/main.py
@@ -6,7 +6,10 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.database import engine, Base, check_db_connection
+
+from app import models
 from app.models import post   # noqa — Base가 Post 테이블을 인식하려면 반드시 import
+from app.routers.post_router import router as post_router
 
 @asynccontextmanager
 async def lifespan(app:FastAPI) :
@@ -25,6 +28,8 @@ description="FastAPI + MariaDB + SQLAlchemy MVC 패턴",
 version="1.0.0",
 lifespan=lifespan
 )
+
+app.include_router(post_router) # 분리된 router 모듈의 APIRouter 객체를 app에 포함
 
 @app.get("/health", tags=["시스템"])
 def health():
