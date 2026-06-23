0948d01c4940e121849b4f386f3272baa59af34f
teadhch <teadhch@naver.com>
2026-06-23 14:04:22 +0900

    feat : class Post stat 테이블 작성

--- app/models/post_model.py
+++ app/models/post_model.py
@@ -3,7 +3,8 @@
 # 역할: DB 테이블 구조만 정의합니다. 쿼리 로직은 여기 없습니다.
 # ============================================================
 
-from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from datetime import datetime
 from app.database import Base
 
@@ -22,3 +23,39 @@ class Post(Base):
         onupdate=datetime.now,   # UPDATE 실행 시 자동으로 현재 시각 갱신
         comment="수정 시각"
     )
+    stat = relationship(
+        "PostStat",
+        back_populates = "post",
+        uselist=False,  # 1대1
+        cascade="all, delete-orphan"
+    )
+    attachments = relationship(
+        "Attachment", 
+        back_populates="post",
+        uselist=True,   # 1대 다
+        cascade="all, delete-orphan"    # 부모 삭제시 첨부파일도 자동 삭제
+        )  
+class PostStat(Base):
+    __tablename__ = "post_stat"
+    # unique=True -> 1대1 보장 (Post - PostStat의 관계가 1:1)
+    id         = Column(Integer, primary_key=True, autoincrement=True)
+    post_id    = Column(Integer, ForeignKey("post.id"),
+                        unique=True, nullable=False)   # unique=True → 1대1 보장
+    like_count = Column(Integer, default=0, comment="좋아요 수")
+    created_at = Column(DateTime, default=datetime.now)
+
+    post = relationship(
+        "Post",
+        back_populates = "post_stat"
+    )
+
+class Attachment(Base):
+    __tablename__ = "attachment"
+    # unique=None -> 1대N (Post - Attachment의 관계가 1:N)
+    id         = Column(Integer, primary_key=True, autoincrement=True)
+    post_id    = Column(Integer, ForeignKey("post.id"), nullable=False)
+    filename   = Column(String(255), nullable=False, comment="파일명")
+    created_at = Column(DateTime, default=datetime.now)
+
+    post = relationship("Post", back_populates="attachments")
+
